203f12ea20aa576b5abd167da750a71110ea553d
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-10 23:00:04 +0530

    operators commit

--- Module1.py
+++ Module1.py
@@ -100,3 +100,143 @@
 # Output:
 # Enter your name:sharvari
 # Your name is sharvari
+
+
+
+
+#Type Conversion
+
+#Implicit Type Conversion
+# a = 10
+# b = 2.5
+# result = a + b
+# print(result)
+# print(type(result))
+
+# Output:
+# 12.5
+# <class 'float'>
+
+
+#Explicit Type Conversion
+# x = "10"
+# y = int(x)
+# print(y + 5)
+
+# Output:
+# 15
+
+
+
+#Operators In Python
+
+#Arithmetic Operator
+# a = 10
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+# Output:
+# 13
+# 7
+# 30
+# 3.3333333333333335
+# 3
+# 1
+# 1000
+
+
+#Assignment Operator
+# x = 10 
+# x += 5
+# print(x)
+
+# Output:
+# 15
+
+
+#Comparison Operator
+# a = 10
+# b = 5
+# print(a == b)
+# print(a != b)
+# print(a > b)
+
+# Output:
+# False
+# True
+# True
+
+
+#Logical Operator
+# age = 20
+# marks = 80
+# print(age >= 18 and marks >= 50)
+# print(age >= 18 or marks < 50)
+# print(not age >= 18)
+
+# Output:
+# True
+# True
+# False
+
+
+#Bitwise Operator
+# a = 5  #Binary:0101
+# b = 3  #Binary:0011
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+# print(a << 1)
+# print(a >> 1)
+
+# #Output:
+# 1
+# 7
+# 6
+# 10
+# 2
+
+
+#Membership Operator
+# name = "Python"
+# print("P" in name)
+# print("z" not in name)
+
+# Output:
+# True
+# True
+
+
+#Identity Operator
+# a = [10 , 20]
+# b = a
+# c = [10 , 20]
+# print(a is b)
+# print(a is c)
+# print(a == b)
+
+# Output:
+# True
+# False
+# True
+
+
+#Operator Precedence
+# result = 10 + 5 * 2
+# print(result)
+
+# Output:
+# 20
+
+
+# result = (10 + 5) * 2
+# print(result)
+
+# Output:
+# 30
