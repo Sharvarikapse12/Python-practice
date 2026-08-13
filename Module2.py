0621d7d1b7a18252ce7388c5e90e857c18ea54aa
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-13 22:45:53 +0530

    comparison operator commit

--- Module2.py
+++ Module2.py
@@ -8,3 +8,21 @@
 
 # Output:
 # Pass
+
+
+
+#Important Operators used in Conditions
+
+#1 Comparison operator
+# a = 10
+# b = 5
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+
+# Output:
+# True
+# False
+# False
+# True
