e4b1ff8875b29d68ad14bc815f383d86b9f93938
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-14 22:36:42 +0530

    syntax and 1 eg of is-statement commit

--- Module2.py
+++ Module2.py
@@ -58,3 +58,19 @@
 
 # Output:
 # It gives IndentationError
+
+
+
+#if-statement
+
+#syntax
+# if condition:
+#     statements
+
+#Example
+# age = 20
+# if age >= 18:
+#     print("You are eligible to vote")
+
+# Output:
+# You are eligible to vote
