f40693d9bd7b118a38d8d4424453e2139149e3bc
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-17 22:34:24 +0530

    problem 6 commit

--- Module2.py
+++ Module2.py
@@ -292,3 +292,14 @@
 
 # output:
 # Stop
+
+
+#6 Check leap year
+# year = 2024
+# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+
+# output:
+# Leap year
