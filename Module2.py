dd2db4c67337d7d91d866aff63afb4ce30ad077f
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-19 21:56:40 +0530

    problem que11 commit

--- Module2.py
+++ Module2.py
@@ -465,3 +465,14 @@
 #     print("Invalid signal")
 
 # output: Go
+
+
+#11 Check whether a year is a leap year or not
+# year = 2024
+# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+
+# output:
+# Leap year
