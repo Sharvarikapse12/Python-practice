81bf76efdb196c270cc0d8abd24e0d2002370e09
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-20 22:27:09 +0530

    problem que21 commit

--- Module2.py
+++ Module2.py
@@ -586,3 +586,18 @@
 
 # output:
 # b is smaller
+
+
+#21 find largest of three number
+# a = 10
+# b = 20
+# c = 30
+# if a >= b and a >= c:
+#     print("a is greater")
+# elif b >= a and b >= c:
+#     print("b is greater")
+# else:
+#     print("c is greater")
+
+# output:
+# c is greater
