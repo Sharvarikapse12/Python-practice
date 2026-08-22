2112fb88835dd320751928f282e275687e2d75b3
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-22 21:24:27 +0530

    que 4 commit

--- Module3.py
+++ Module3.py
@@ -168,3 +168,19 @@
 
 # output:
 # Factorial: 1
+
+
+#4 Prime number check
+# num = 7
+# count = 0
+# for i in range(1,num+1):
+#     if num % i == 0:
+#         count = count + 1
+
+# if count == 2:
+#     print("Prime number")
+# else:
+#     print("Not prime")
+
+# output:
+# Prime number
