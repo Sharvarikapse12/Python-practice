4fd2cb32b3457252e20271da365fb9fe01ca18bb
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-25 23:23:59 +0530

    que 10 commit

--- Module3.py
+++ Module3.py
@@ -253,3 +253,18 @@
 # ***
 # ****
 # *****
+
+
+#10 print number pattern
+# n = 5
+# for i in range(1 , n + 1):
+#     for j in range(1 , i + 1):
+#         print(j , end=" ")
+#     print()
+
+# output:
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
