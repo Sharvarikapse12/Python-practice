43a1a5922006e86ef38aaf15b2c8007c87876e39
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-08-25 23:16:09 +0530

    que 7 commit

--- Module3.py
+++ Module3.py
@@ -208,3 +208,21 @@
 
 # output:
 # Reverse : 4321
+
+
+#7 check palindrome number
+# num = 101
+# temp = num
+# rev = 0
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num = num // 10
+
+# if temp == rev:
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+
+# output:
+# Palindrome
