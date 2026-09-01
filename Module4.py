814faae4b695f1c1b67d46ec8c05b88b9678fbd9
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-09-01 22:53:10 +0530

    eg of string commit

--- Module4.py
+++ Module4.py
@@ -1,2 +1,7 @@
 #String
 #string is a sequence od character . In python, strings are written inside quotes , double quotes or triple quotes 
+
+#Example
+# name = "Python"
+# message = 'Welcome'
+# paragraph = """ This is a multi-line string."""
